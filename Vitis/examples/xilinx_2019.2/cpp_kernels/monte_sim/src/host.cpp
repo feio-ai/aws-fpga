4e6e1831d9b228196f2208171e8d8372294bba94
Allen VanNoppen Jr <avnjr@Allens-MacBook-Pro.local>
2020-04-23 15:56:49 -0500

    type cast

--- Vitis/examples/xilinx_2019.2/cpp_kernels/monte_sim/src/host.cpp
+++ Vitis/examples/xilinx_2019.2/cpp_kernels/monte_sim/src/host.cpp
@@ -81,7 +81,7 @@ int main(int argc, char **argv) {
     // Create the test data
     std::generate(source_in1.begin(), source_in1.end(), rand_fix_gen);
     // std::generate(source_in_sw.begin(), source_in_sw.end(), );
-    fix_type x;
+    fix_type *x;
     for (int i = 0; i < DATA_SIZE; i++) {
         // source_sw_results[i] = source_in1[i] + source_in2[i];
         x = source_in1[i];
