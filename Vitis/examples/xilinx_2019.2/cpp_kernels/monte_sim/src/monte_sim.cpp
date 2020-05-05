8e87d74b31670e7499ebbb7951dd5fa8b5bf47da
Allen VanNoppen Jr <avnjr@Allens-MacBook-Pro.local>
2020-05-05 17:51:55 -0500

    buffer error

--- Vitis/examples/xilinx_2019.2/cpp_kernels/monte_sim/src/monte_sim.cpp
+++ Vitis/examples/xilinx_2019.2/cpp_kernels/monte_sim/src/monte_sim.cpp
@@ -30,89 +30,87 @@ void monte_sim(
                 fix_type *in2,
                 fix_type *out_r,
                 int size
-    ) {
+) {
 
-        // Buffer inputs
-        #pragma HLS INTERFACE m_axi port=in1 offset=slave bundle=gmem
-        #pragma HLS INTERFACE m_axi port=in2 offset=slave bundle=gmem
-        #pragma HLS INTERFACE m_axi port=out_r offset=slave bundle=gmem
-        #pragma HLS INTERFACE s_axilite port = in1 bundle = control
-        #pragma HLS INTERFACE s_axilite port = in2 bundle = control
-        #pragma HLS INTERFACE s_axilite port = out_r bundle = control
-        #pragma HLS INTERFACE s_axilite port = size bundle = control
-        #pragma HLS INTERFACE s_axilite port = return bundle = control
+// Buffer inputs
+#pragma HLS INTERFACE m_axi port=in1 offset=slave bundle=gmem
+#pragma HLS INTERFACE m_axi port=in2 offset=slave bundle=gmem
+#pragma HLS INTERFACE m_axi port=out_r offset=slave bundle=gmem
+#pragma HLS INTERFACE s_axilite port = in1 bundle = control
+#pragma HLS INTERFACE s_axilite port = in2 bundle = control
+#pragma HLS INTERFACE s_axilite port = out_r bundle = control
+#pragma HLS INTERFACE s_axilite port = size bundle = control
+#pragma HLS INTERFACE s_axilite port = return bundle = control
 
-            fix_type v1_buffer[BUFFER_SIZE];
-            fix_type v2_buffer[BUFFER_SIZE];
-            fix_type vout_buffer[BUFFER_SIZE];
+    fix_type v1_buffer[BUFFER_SIZE];
+    fix_type v2_buffer[BUFFER_SIZE];
+    fix_type vout_buffer[BUFFER_SIZE];
+
+    // Read Constants loop
+    for (int z = 0; z < 4; z++) {
+        #pragma HLS LOOP_TRIPCOUNT min=c_size max=c_size
+        #pragma HLS PIPELINE II=1
+        v2_buffer[z] = in2[i + z];
+    }
+    
+    fix_type t = v2_buffer[0];
+    fix_type so = v2_buffer[1];
+    fix_type r = v2_buffer[2];
+    fix_type sig = v2_buffer[3];
+
+    for (int i = 0; i < size; i += BUFFER_SIZE) {
+        #pragma HLS LOOP_TRIPCOUNT min=c_len max=c_len
+        int chunk_size = BUFFER_SIZE;
+
+        if ((i + BUFFER_SIZE) > size)
+            chunk_size = size - i;
 
 
-            for (int i = 0; i < size; i += BUFFER_SIZE) {
-                #pragma HLS LOOP_TRIPCOUNT min=c_len max=c_len
-                int chunk_size = BUFFER_SIZE;
-
-                if ((i + BUFFER_SIZE) > size)
-                    chunk_size = size - i;
-
-
-            read1:
-                for (int j = 0; j < chunk_size; j++) {
-                    #pragma HLS LOOP_TRIPCOUNT min=c_size max=c_size
-                    #pragma HLS PIPELINE II=1
-                    v1_buffer[j] = in1[i + j];
-                }
-
-            read2:
-                for (int j = 0; j < 4; j++) {
-                    #pragma HLS LOOP_TRIPCOUNT min=c_size max=c_size
-                    #pragma HLS PIPELINE II=1
-                    v2_buffer[j] = in2[i + j];
-                }
-
-                fix_type t = v2_buffer[0];
-                fix_type so = v2_buffer[1];
-                fix_type r = v2_buffer[2];
-                fix_type sig = v2_buffer[3];
-
+    read1:
+        for (int j = 0; j < chunk_size; j++) {
+            #pragma HLS LOOP_TRIPCOUNT min=c_size max=c_size
+            #pragma HLS PIPELINE II=1
+            v1_buffer[j] = in1[i + j];
+        }
                 // PIPELINE pragma reduces the initiation interval for loop by allowing the
                 // concurrent executions of operations
 
-                fix_type duo = 2;
-                fix_type trio = 3;
-                fix_type hls_p = hls::pow(sig, duo);
-                fix_type hls_sq = hls::sqrt(t);
-            monte_sim:
-                for (int j = 0; j < chunk_size; j++) {
-                    #pragma HLS LOOP_TRIPCOUNT min=c_size max=c_size
-                    #pragma HLS PIPELINE II=1
+        fix_type duo = 2;
+        fix_type trio = 3;
+        fix_type hls_p = hls::pow(sig, duo);
+        fix_type hls_sq = hls::sqrt(t);
+    monte_sim:
+        for (int j = 0; j < chunk_size; j++) {
+            #pragma HLS LOOP_TRIPCOUNT min=c_size max=c_size
+            #pragma HLS PIPELINE II=1
 
-                    fix_type x = v1_buffer[j];
+            fix_type x = v1_buffer[j];
                     
-                    fix_type xo = (r - ( hls_p / duo ) * t) + ( x * sig * hls_sq);
-                    fix_type x2 = hls::pow(xo, duo);
-                    fix_type x3 = hls::pow(xo, trio);
-                    fix_type x4 = hls::pow(x2, duo);
-                    fix_type x5 = hls::pow(x2, trio);
-                    fix_type x6 = hls::pow(x4, duo);
+            fix_type xo = (r - ( hls_p / duo ) * t) + ( x * sig * hls_sq);
+            fix_type x2 = hls::pow(xo, duo);
+            fix_type x3 = hls::pow(xo, trio);
+            fix_type x4 = hls::pow(x2, duo);
+            fix_type x5 = hls::pow(x2, trio);
+            fix_type x6 = hls::pow(x4, duo);
                     
 
-                    fix_type exp_result = 1 + xo + (x2 * ov_2) + (x3 * ov_6) + (x4 * ov_24) + (x5 * ov_120) + (x6 * ov_720);
-                    fix_type s = so * exp_result;
-                    vout_buffer[j] = s;
-                }
+            fix_type exp_result = 1 + xo + (x2 * ov_2) + (x3 * ov_6) + (x4 * ov_24) + (x5 * ov_120) + (x6 * ov_720);
+            fix_type s = so * exp_result;
+            vout_buffer[j] = s;
+        }
 
             //burst write the result
-            write:
-                for (int j = 0; j < chunk_size; j++) {
-                    #pragma HLS LOOP_TRIPCOUNT min=c_size max=c_size
-                    #pragma HLS PIPELINE II=1
-                    out_r[i + j] = vout_buffer[j];
-                }
+    write:
+        for (int j = 0; j < chunk_size; j++) {
+            #pragma HLS LOOP_TRIPCOUNT min=c_size max=c_size
+            #pragma HLS PIPELINE II=1
+            out_r[i + j] = vout_buffer[j];
+        }
 
-            }
-        
     }
-
-
+        
 }
+
+
+
 
