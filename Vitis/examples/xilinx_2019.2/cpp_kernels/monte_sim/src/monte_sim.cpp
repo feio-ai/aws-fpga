86eeb29a24a791210fdfff5278f1a7425716ada1
Allen VanNoppen Jr <avnjr@Allens-MacBook-Pro.local>
2020-05-20 21:42:11 -0500

    Modify for Asian Options

--- Vitis/examples/xilinx_2019.2/cpp_kernels/monte_sim/src/monte_sim.cpp
+++ Vitis/examples/xilinx_2019.2/cpp_kernels/monte_sim/src/monte_sim.cpp
@@ -39,7 +39,7 @@ void monte_sim(
     fix_type v2_buffer[NUM_STEPS];
     fix_type vout_buffer[NUM_STEPS][BUFFER_SIZE];
 
-    #pragma HLS ARRAY_PARTITION variable = v1_buffer dim = 2 block factor = 2
+    #pragma HLS ARRAY_PARTITION variable = v1_buffer dim = 2 block factor = 10
     
 // Read Constants loop
 read_const:
