a77bee51c1e1a0f84a29cc0e66e0ad3b05387435
Allen VanNoppen Jr <avnjr@Allens-MacBook-Pro.local>
2020-05-22 13:54:34 -0500

    Kernel Partition

--- Vitis/examples/xilinx_2019.2/cpp_kernels/monte_sim/src/monte_sim.cpp
+++ Vitis/examples/xilinx_2019.2/cpp_kernels/monte_sim/src/monte_sim.cpp
@@ -5,12 +5,12 @@
 #include "hls_math.h"
 #include "ap_fixed.h"
 
-#define NUM_STEPS 100
+#define NUM_STEPS 10
 #define BUFFER_SIZE 10000
-#define DATA_SIZE 100000
+#define DATA_SIZE 10000
 #define CONST_SIZE 4
 
-typedef ap_fixed<16,8> fix_type;
+typedef ap_fixed<32,16> fix_type;
 
 //TRIPCOUNT identifiers
 // const unsigned int c_len = (DATA_SIZE * NUM_STEPS) / BUFFER_SIZE;
